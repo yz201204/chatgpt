99b41b5e657b9e4b9d6d74c56554621273833df1
yz201204 <yangzhe@qijiayoudao.com>
2023-02-18 14:33:15 +0800

    cha

--- config/api_key.py
+++ config/api_key.py
@@ -1,3 +1,4 @@
 api_key = "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"
 api_key = "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"
 api_key = "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"
+api_key = "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"
