ff058c81916c2c694484bb590c312f69c823a88b
yz201204 <yangzhe@qijiayoudao.com>
2023-02-20 01:19:02 +0800

    cha

--- config/api_key.py
+++ config/api_key.py
@@ -1,2 +1,2 @@
 api_key = "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"
-api_key = "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"
+api_key = "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"
