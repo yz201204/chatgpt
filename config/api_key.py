4af412049af31d40de42ecee1ef58a6a7daabe61
yz201204 <yangzhe@qijiayoudao.com>
2023-02-20 01:23:25 +0800

    cha

--- config/api_key.py
+++ config/api_key.py
@@ -1,3 +1,1 @@
-api_key = "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"
-api_key = "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"
 api_key = "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"
