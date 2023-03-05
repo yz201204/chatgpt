94cf6504d1c6f5781f44c0365e4aa04a784b1ebe
yz201204 <yangzhe@qijiayoudao.com>
2023-03-06 00:18:12 +0800

    cha3.5

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 JENKINS_SSH_PATH=$1
 
-nohup python3 $JENKINS_SSH_PATH/run_chat_api_server.py >> $JENKINS_SSH_PATH/nohup.out  2>&1 &
+nohup python3.9 $JENKINS_SSH_PATH/run_chat_api_server.py >> $JENKINS_SSH_PATH/nohup.out  2>&1 &
